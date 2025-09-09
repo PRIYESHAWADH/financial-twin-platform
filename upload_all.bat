713d45c9607f531db7384cec44c3b73df0919d17
PRIYESHAWADH <priyeshawadh@gmail.com>
2025-09-09 08:39:45 +0530

    Remove upload_all.bat file and update project structure

--- upload_all.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Uploading all files to GitHub...
-git add .
-git commit -m "Complete FinTwin platform upload with all files"
-git push origin main --force
-echo Upload complete!
-pause
